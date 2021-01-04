26ab4324636a80921e6dae2bae172db305943f3d
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-04 21:36:22 +0000

    cosmetic update to logs on server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
 import app from './app';
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('');
+  console.log('🐺🤟');
+  console.log('Server started on port 3333!');
 });
