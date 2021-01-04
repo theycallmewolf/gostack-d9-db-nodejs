13b15064086609bea370ec8868cbe25a0daa0a9b
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-04 21:40:08 +0000

    feat: adds costumers controller

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
-import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+    const createCostumer = container.resolve(CreateCustomerService);
+    const costumer = await createCostumer.execute({ name, email });
+
+    return response.json(costumer);
   }
 }
