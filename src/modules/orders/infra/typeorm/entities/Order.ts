62a05f388525398832d0a6b08d7ac9e530134e5a
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-05 00:15:30 +0000

    feat: adds entities to products, orders and orders products

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -6,20 +6,33 @@ import {
   ManyToOne,
   JoinColumn,
   OneToMany,
+  Column,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  costumer_id: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'costumer_id' })
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: ["insert"]
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
