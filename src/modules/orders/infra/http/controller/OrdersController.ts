24e1e51d42f7521f2215699092012f610b823ade
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-06 23:15:09 +0000

    adds show and create methods to orders controller

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -4,13 +4,23 @@ import { container } from 'tsyringe';
 
 import CreateOrderService from '@modules/orders/services/CreateOrderService';
 import FindOrderService from '@modules/orders/services/FindOrderService';
+import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { id } = request.params;
+    const findOrder = container.resolve(FindOrderService);
+    const order = await findOrder.execute({ id });
+    return response.json(order);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = request.body;
+    const createOrder = container.resolve(CreateOrderService);
+    const order = await createOrder.execute({
+      customer_id,
+      products
+    });
+    return response.json(order);
   }
 }
